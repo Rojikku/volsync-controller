643b3bf347b85fc35ace0d302cf5be030ea0211c
Rojikku <Rojikku@users.noreply.github.com>
2023-06-25 23:25:57 +0000

    feat: Detected volsync controller

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"path/filepath"
 
 	// "time"
 
@@ -30,7 +29,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -52,14 +50,15 @@ func main() {
 
 	var config *rest.Config
 	var err error
+	var volsyncNamespace string = "volsync"
 
 	if externalTest {
-		var kubeconfig *string
-		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
+		kubeconfig := flag.String("kubeconfig", ".kubeconfig", "(optional) absolute path to the kubeconfig file")
+		// if home := homedir.HomeDir(); home != "" {
+		// 	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		// } else {
+		// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		// }
 
 		// use the current context in kubeconfig
 		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -76,28 +75,47 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	// Examples for error handling:
-	// - Use helper functions like e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	namespace := "volsync"
-	pod := "volsync"
-	_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+	// Query pods
+	pods, err := clientset.CoreV1().Pods(volsyncNamespace).List(context.TODO(), metav1.ListOptions{})
 	if errors.IsNotFound(err) {
-		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		fmt.Printf("Unable to find volsync pod in %s namespace\n", volsyncNamespace)
 	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-			pod, namespace, statusError.ErrStatus.Message)
+		fmt.Printf("Error getting pod in namespace %s: %v\n",
+			volsyncNamespace, statusError.ErrStatus.Message)
 	} else if err != nil {
 		panic(err.Error())
-	} else {
-		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
 	}
+	fmt.Printf("There are %d pods in %s\n", len(pods.Items), volsyncNamespace)
 
-	// time.Sleep(10 * time.Second)
+	// Verify the volsync pod is running
+	if len(pods.Items) >= 1 {
+		match := false
+		for _, pod := range pods.Items {
+			if pod.Name[:7] == "volsync" {
+				fmt.Printf("Pod %s in namespace %s found\n", pod.Name, pod.Namespace)
+				match = true
+			}
+		}
+		if !match {
+			fmt.Printf("volsync pod not found???\n")
+		}
+	} else {
+		panic("No pods found")
+	}
+		
+	// fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// List all instances of replicationsource crd
+	// FIXME: This is unmarshaled json or something and I don't know how to fix it, but it returns data.
+	data, err := clientset.RESTClient().Get().AbsPath("/apis/volsync.backube/v1alpha1/replicationsources"). //Namespace("default").
+	DoRaw(context.TODO())
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		panic(err)
+	}
+	// id := struct{}
+	// data = json.Unmarshal(data, &id)
+	fmt.Printf("%v", data)
+
 }
